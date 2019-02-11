566a31cb402ca446c6049ad32a653f0b13b25cae
EeliH <4096394+EeliH.noreply.github.com>
2019-02-11 16:03:53 +0200

    yeah

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>Hello World!</div>
+  <div>Hello World! I hope you answer back to me.</div>
 );
 
 class App extends Component {
